624fef7ff297aa7fe332422d84d3fb1064d75297
anthony2445 <anthony2445@live.ca>
2018-04-08 02:13:59 -0400

    Still trying to get news stories to show on the news page, not sure if this is even necessary at this point
    Signed-off-by: anthony2445 <anthony2445@live.ca>

--- base_files/lab10.js
+++ base_files/lab10.js
@@ -63,8 +63,56 @@ app.get('/checkUsername', function (req, res) {
 });
 
 app.get('/news', function (req, res) {
-    // render the 'enterUsername' template, and pass in a few variables
+    // render the 'news' template, and pass in a few variables
     res.render('news2', { title: 'News Page', message: 'News should be below' });
+
+		var topStory= document.getElementById("topStories");
+		var req1;
+		var request = new XMLHttpRequest();
+
+		$( "#topStories" ).accordion();
+
+	    request.onreadystatechange = function() {
+				//console.log(request.readyState);
+				//console.log(request.status);
+	      if (request.readyState == 4 && request.status == 200) {
+					req1 = request.responseXML
+					for(var i =0; i <2; i++)
+					{
+	        	var title = req1.getElementsByTagName('entry')[i].getElementsByTagName('title')[0].childNodes[0].nodeValue;
+						var txt1 = $("<h3></h3>").text(title);   // Create with jQuery
+						//$('#topStories').append(txt1);
+
+						var updated = req1.getElementsByTagName('entry')[i].getElementsByTagName('updated')[0].childNodes[0].nodeValue;
+						var txt2 = $("<h4></h4>").text(updated);   // Create with jQuery
+						//$('#topStories').append(txt2);
+
+						var summary = req1.getElementsByTagName('entry')[i].getElementsByTagName('summary')[0].childNodes[0].nodeValue;
+						var txt3 = $("<p></p>").text(summary);   // Create with jQuery
+
+						//var div = $("<div></div>").text(txt1, txt2, txt3);
+
+						//$('#topStories').append(div);
+						console.log('1');
+						var content = req1.getElementsByTagName('entry')[i].getElementsByTagName('content')[0].childNodes[0].nodeValue;
+						var txt4 = $("<p></p>").text(content);   // Create with jQuery
+						$('#topStories').append(txt1);
+						$('#topStories').accordion("refresh");
+
+						jQuery('<div/>', {
+						    id: 'foo' + i,
+						}).appendTo('#topStories');
+						$('#foo' + i).append(txt2, txt3, txt4);
+					}
+					$('#topStories').accordion("refresh");
+					//console.log(title)
+	    		//topStory.innerHTML = title;
+					//$("body").append(txt3);
+	      }
+	    };
+	    request.open('GET', 'topstories.atom', true);
+	    request.send();
+
 });
 
 app.get('/help', function (req, res) {
